79bdcbe7243480533fe69c1f821e82b959f8b2f2
YoonTaeminnnn <xoals4340@hs.ac.kr>
2021-07-17 15:24:32 +0000

    practice

---
+++ greedy/g3.py
@@ -0,0 +1,19 @@
+n,m,k=map(int,input().split())
+arr = list(map(int,input().split()))
+
+arr.sort()
+cnt=0
+result = 0
+first = arr[n-1]
+second = arr[n-2]
+
+
+for _ in range(m):
+  cnt+=1
+  if cnt <= k:
+    result+=first
+    continue
+  result+=second
+  cnt=0
+
+print(result)
