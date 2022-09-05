7ebb5c1fb241cf2b2aef9947e23c25cfdeaeb5b0
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-09-05 06:37:33 +0000

    bj

--- greedy/10162.py
+++ greedy/10162.py
@@ -1,5 +1,6 @@
 second = int(input())
 
+# // 몫만 출력
 a = second // 300
 second%=300
 b = second // 60
@@ -7,7 +8,7 @@ second%=60
 c = second // 10
 second%=10
 
-if second%60 != 0:
+if second != 0:
   print(-1)
 else:
   print(a,b,c)
--- test.py
+++ test.py
@@ -1,24 +1,41 @@
-def solution(N, stages):
-    answer = []
-    mapping = []
-    for i in range(1,N+1):  #1~5
-        cnt = 0
-        s = 0
-        result = 0
-        for j in stages:       #1~6
-            if j >= i :
-                s+=1
-            if j == i:
-                cnt+=1
-        if s == 0:
-            result = 0
-            continue
-        result = cnt / s
-        mapping.append((i,result))  #(1,실패율), (2,실패율)
-    mapping.sort(key = lambda x : x[1], reverse=True)
-    print(mapping)
-    for i in mapping:
-        answer.append(i[0])
-    return answer
+from collections import deque
+from itertools import combinations
+import sys, copy
 
+input = sys.stdin.readline
 
+def bfs():
+    c = set()
+    ans = 0
+    qlen = len(q)
+    while qlen:
+        x = q.popleft()
+        l = list(str(x))
+        for i, j in d:
+            s = copy.deepcopy(l)
+            temp_i, temp_j = s[i], s[j]
+            s[i], s[j] = temp_j, temp_i
+            if s[0] == '0':
+                continue
+            nx = int(''.join(s))
+            if nx not in c:
+                ans = max(ans, nx)
+                c.add(nx)
+                q.append(nx)
+        qlen -= 1
+    return ans
+
+n, k = map(int, input().split())
+item = [i for i in range(len(str(n)))]
+d = list(combinations(item, 2))
+q = deque()
+q.append(n)
+
+ans = 0
+while k:
+    ans = bfs()
+    k -= 1
+if not ans:
+    print(-1)
+else:
+    print(ans)
