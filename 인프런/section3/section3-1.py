12b5b864502d7dd6cca8481b46f458b236dfb415
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-09-22 17:48:51 +0000

    9/23

--- 인프런/section3/section3-1.py
+++ 인프런/section3/section3-1.py
@@ -1,3 +1,4 @@
+#---------슬라이싱 이용---------
 n = int(input())
 for i in range(1,n+1):
   word = input()
@@ -6,4 +7,19 @@ for i in range(1,n+1):
     print('#%d YES' %i)
   else:
     print('#%d NO' %i)
+
+#----직접 구현------
+
+n = int(input())
+for i in range(1,n+1):
+  word = input()
+  word = word.lower()
+  index = len(word) // 2
+  for j in range(index):
+    if word[j] != word[-1-j]:
+      print('#%d NO' %i)
+      break
+  else:
+    print('#%d YES' %i)
+  
     
