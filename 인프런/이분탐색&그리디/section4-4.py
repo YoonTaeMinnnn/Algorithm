c820e69358be3969b6c6433a0f09f210286c2d66
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-10-13 07:49:11 +0000

    10/13

---
+++ 인프런/이분탐색&그리디/section4-4.py
@@ -0,0 +1,28 @@
+def hourse(m):
+    a = l[0]
+    cnt = 1
+    for i in range(1, n):
+        if l[i] - a >= m:
+            a = l[i]
+            cnt += 1
+    if cnt < m:
+        return False
+    return True
+
+
+n, c = map(int, input().split())
+l = []
+for _ in range(n):
+    l.append(int(input()))
+l.sort()
+lt = 1
+rt = l[n - 1]
+res = 0
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if hourse(mid):
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+print(res)
