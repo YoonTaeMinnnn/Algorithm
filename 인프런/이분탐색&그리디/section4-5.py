826f81e56a1d1dee02134e61e889fe2efee6db20
YoonTaeminnnn <xoals4340@hs.ac.kr>
2022-10-15 16:12:48 +0000

    10/16

---
+++ 인프런/이분탐색&그리디/section4-5.py
@@ -0,0 +1,14 @@
+n = int(input())
+l = []
+for _ in range(n):
+  l.append(list(map(int, input().split())))
+l = sorted(l, key=lambda x:(x[1], x[1]-x[0]))
+
+a = l[0][1]
+cnt = 1
+for i in range(1,n):
+  if a <= l[i][0]:
+    a = l[i][1]
+    cnt += 1
+
+print(cnt)
