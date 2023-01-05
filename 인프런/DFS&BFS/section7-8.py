52695c8e4cad17201d5763e0379f7e303b5e228c
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-01-05 19:03:11 +0000

    1/6

--- 인프런/DFS&BFS/section7-8.py
+++ 인프런/DFS&BFS/section7-8.py
@@ -1,10 +1,10 @@
 from collections import deque
-import sys
 
 n = int(input())
 l = [list(map(int, input().split())) for _ in range(n)]
 center = l[n//2][n//2]
 ch = [[0] * n for _ in range(n)]
+ch[n//2][n//2] = 1
 queue = deque()
 queue.append((n//2, n//2))
 sum = center
@@ -15,14 +15,16 @@ L = 0
 while True:
   if L == n//2:
     break
-  x, y = queue.popleft()
-  print(sum)
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    if ch[nx][ny] == 0:
-      ch[nx][ny] = 1
-      sum += l[nx][ny]
-      queue.append((nx,ny))
+  for _ in range(len(queue)):
+    x, y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+  
+      if ch[nx][ny] == 0:
+        print(nx, ny)
+        ch[nx][ny] = 1
+        sum += l[nx][ny]
+        queue.append((nx,ny))
   L+=1
 print(sum)
