870499a68ccecc241e0d9cb8c516325adddf33fe
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-01-22 15:57:05 +0000

    1/23

--- 인프런/DFS&BFS/section7-14.py
+++ 인프런/DFS&BFS/section7-14.py
@@ -4,48 +4,41 @@ n = int(input())
 l = [list(map(int, input().split())) for _ in range(n)]
 max = 0
 for i in range(n):
-  for j in range(n):
-    if l[i][j] > max:
-      max = l[i][j]
+    for j in range(n):
+        if l[i][j] > max:
+            max = l[i][j]
 queue = deque()
 result = []
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 for height in range(1, max):
-  ch = [[0]*n for _ in range(n)]
-  cnt = 0
-  for i in range(n):
-    for j in range(n):
-      if l[i][j] > height and ch[i][j] == 0:
-        cnt += 1
-        ch[i][j] = 1
-        queue.append((i,j))
-        while queue:
-          x, y = queue.popleft()
-          for z in range(4):
-            nx = x + dx[z]
-            ny = y + dy[z]
+    ch = [[0] * n for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if l[i][j] > height and ch[i][j] == 0:
+                cnt += 1
+                ch[i][j] = 1
+                queue.append((i, j))
+                while queue:
+                    x, y = queue.popleft()
+                    for z in range(4):
+                        nx = x + dx[z]
+                        ny = y + dy[z]
 
-            if 0<=nx<n and 0<=ny<n and l[nx][ny] > height and ch[nx][ny] == 0:
-              ch[nx][ny] = 1
-              queue.append((nx,ny))
-  
-            
-  result.append(cnt)
+                        if 0 <= nx < n and 0 <= ny < n and l[nx][
+                                ny] > height and ch[nx][ny] == 0:
+                            ch[nx][ny] = 1
+                            queue.append((nx, ny))
 
+    result.append(cnt)
 
 max = 0
 for i in result:
-  if i > max:
-    max = i
+    if i > max:
+        max = i
 print(max)
-
-
-
-  
 
 # 68262 32346 67332 72536 89527
---
+++ 인프런/DFS&BFS/section7-15.py
@@ -0,0 +1,43 @@
+from collections import deque
+import sys
+
+m, n = map(int, input().split())
+l = [list(map(int, input().split())) for _ in range(n)]
+
+# ch = [[0]*m for _ in range(n)]
+queue = deque()
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+
+for i in range(n):
+  for j in range(m):
+    if l[i][j] == 1:
+      queue.append((i,j))
+
+while queue:
+  x, y = queue.popleft()
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    if 0<=nx<n and 0<=ny<m and l[nx][ny] == 0:
+      l[nx][ny] = l[x][y] + 1
+      queue.append((nx,ny))
+
+res = -2147000000
+for i in range(n):
+  for j in range(m):
+    if l[i][j] == 0:
+      print(-1)
+      sys.exit(0)
+    if l[i][j] > res:
+      res = l[i][j]
+
+if res == 1:
+  print(0)
+else:
+  print(res-1)
+
+
