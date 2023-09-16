8456804b47a14f2700d066031bf816a608ee72e9
YoonTaeminnnn <xoals4340@gmail.com>
2023-09-16 15:07:01 +0000

    9/17

---
+++ 구름/투포인터.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+l = list(map(int, input().split()))
+
+count = 0
+start = 0
+end = 1
+if l[start] == M:
+  count += 1
+s = l[start]
+
+while end <= N:
+  if 
+
+print(count)
