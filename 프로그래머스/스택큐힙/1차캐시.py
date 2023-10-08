b14fc5342b83bdbf740c3a45225089bb4e85dfcb
YoonTaeminnnn <xoals4340@gmail.com>
2023-10-08 06:24:35 +0000

    10/8

---
+++ 프로그래머스/스택큐힙/1차캐시.py
@@ -0,0 +1,22 @@
+def solution(cacheSize, cities):
+    answer = 0
+    
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    cache = []
+    for city in cities:
+        city = city.upper()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            if len(cache) == cacheSize:
+                cache.pop(0)
+            cache.append(city)
+            answer += 5
+    
+                
+    
+    return answer
