e31c62a3fc9bd194b6e53276663297c057188709
YoonTaeminnnn <xoals4340@gmail.com>
2023-11-15 17:02:04 +0000

    11/16

---
+++ 프로그래머스/java/dp/멀리뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        int dp[] = new int[n+1];
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<n+1; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        return dp[n] % 1234567;
+    }
+}
